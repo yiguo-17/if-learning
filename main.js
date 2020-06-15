58f3ca359cf205a5411b92815e22d720d120d59c
play.js <git@gituser.com>
2020-06-15 13:59:56 -0400

    add string function

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(str){
+  if(str.length > 20){return "That's a long string!";}
+}
 
+function isItMedium(str){
+  if(str.length >= 10 && str.length<=20){return "That's a regular sized string!";}
+}
 
+function isItShort(str){
+  if(str.length< 10){return "That's a small string!";}
+}
+
+function howLongIsMyString(str){
+  const len = str.length;
+  if(len) > 20){return "That's a long string!";}
+  else if(len >= 10 && len<=20){return "That's a regular sized string!";}
+  else {return "That's a small string!";}
+}
 
 
 
